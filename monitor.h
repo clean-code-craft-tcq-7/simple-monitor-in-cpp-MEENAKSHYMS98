75c3d882b767f4086543589c013ba2d35af95d55
MEENAKSHYMS98 <msmeenakshy98@gmail.com>
2025-07-28 17:31:36 +0530

    Update monitor.h

--- monitor.h
+++ monitor.h
@@ -2,6 +2,6 @@
 
 int vitalsOk(float temperature, float pulseRate, float spo2);
 int vit_Temp(float temperature);
-int vit_Temp(float pulseRate);
+int vit_PulseRate(float pulseRate);
 int vit_Spo2(float spo2);
 void time();
