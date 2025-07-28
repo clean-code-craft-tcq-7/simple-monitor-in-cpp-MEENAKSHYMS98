65acbf570844f95465e734a5809b0d136ffc0e10
MEENAKSHYMS98 <msmeenakshy98@gmail.com>
2025-07-28 17:22:41 +0530

    Update to monitor.cpp for including time function

--- monitor.cpp
+++ monitor.cpp
@@ -9,23 +9,23 @@ int vitalsOk(float temperature, float pulseRate, float spo2) {
 return vit_Temp(temperature)&&vit_PulseRate(pulseRate)&&vit_Spo2(spo2);
 }
  
-int vit_Temp(float temperature){
+int vit_Temp(float temperature) {
   if (temperature > 102 || temperature < 95) {
     cout << "Temperature is critical!\n";
     delay();
       return 0;
     }
     return 1;
-  } 
-  int vit_PulseRate(float pulseRate){
+  }
+  int vit_PulseRate(float pulseRate) {
   if (pulseRate < 60 || pulseRate > 100) {
     cout << "Pulse Rate is out of range!\n";
     delay();
     return 0;
     }
     return 1;
-  } 
-  int vit_Spo2(float spo2){
+  }
+  int vit_Spo2(float spo2) {
     if (spo2 < 90) {
     cout << "Oxygen Saturation out of range!\n";
     delay();
@@ -33,7 +33,7 @@ int vit_Temp(float temperature){
     }
   return 1;
   }
-void delay(){
+void time() {
 for (int i = 0; i < 6; i++) {
       cout << "\r* " << flush;
       sleep_for(seconds(1));
